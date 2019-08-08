d234a15a038af7d0f9cc5ef56f36049afa7b7898
Son-Guhun <22954418+Son-Guhun@users.noreply.github.com>
2019-08-07 22:53:37 -0300

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -11,10 +11,24 @@ require('Commands.Player.deleteme')
 
 require('multipatrol.addons.automatic')
 
-
-
+require('Commands.Decoration.visuals')
+require('Commands.Player.start')
 
 local function a()
+    for i,p in player.iterAll() do
+        local udg_temp_player = p.handle
+        CreateFogModifierRectBJ( true, udg_temp_player, FOG_OF_WAR_VISIBLE,  GetPlayableMapRect())
+        SetPlayerAbilityAvailableBJ( false, FourCC('Apos'), udg_temp_player )
+        SetPlayerAbilityAvailableBJ( false, FourCC('Aps2'), udg_temp_player )
+        SetPlayerAbilityAvailableBJ( false, FourCC('ACps'), udg_temp_player )
+        SetPlayerAbilityAvailableBJ( false, FourCC('Ashm'), udg_temp_player )
+        SetPlayerAbilityAvailableBJ( false, FourCC('Ahid'), udg_temp_player )
+        SetPlayerAbilityAvailableBJ( false, FourCC('Sshm'), udg_temp_player )
+        SetPlayerHandicapXPBJ( udg_temp_player, 0.00 )
+        AdjustPlayerStateBJ( 1000000000, udg_temp_player, PLAYER_STATE_RESOURCE_GOLD )
+        AdjustPlayerStateBJ( 1000000000, udg_temp_player, PLAYER_STATE_RESOURCE_LUMBER )
+        SetPlayerMaxHeroesAllowed( -1, udg_temp_player )
+    end
 end
 
 ceres.addHook("main::after",  a)
